3f048318006928a822eda862bc2119a95856d3a3
Abdullah <abd.eltiby@gmail.com>
2024-12-02 19:42:59 +0300

    Change the project name from intertrguide to travino

--- travino/asgi.py
+++ travino/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for intertrguide project.
+ASGI config for travino project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intertrguide.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travino.settings')
 
 application = get_asgi_application()
